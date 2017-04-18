6078aaecd145cd0479e7e41c1f973a29a8cdacbc
Александр Сорокин <aleksandrsorokin@mbp-aleksandr.rocketsoftware.com>
2017-04-18 13:23:57 +0500

    Initial Commit

---
+++ CoreDataTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoreDataTest
+//
+//  Created by Александр Сорокин on 18.04.17.
+//  Copyright © 2017 Александр Сорокин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
